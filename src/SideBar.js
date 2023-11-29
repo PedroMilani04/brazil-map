4235bcc52c1ab7cf2375586df6ff259d2dad1f40
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2023-11-29 17:49:13 -0300

    css changes

--- src/SideBar.js
+++ src/SideBar.js
@@ -4,13 +4,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faEarthAmericas } from "@fortawesome/free-solid-svg-icons";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import carousel styles
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
-const SideBar = ({ key, selectedState, selectedCapital, selectedBiome, selectedFuso, selectedPopul, Img1, Img2 }) => {
+const SideBar = ({ selectedState, selectedCapital, selectedBiome, selectedFuso, selectedPopul, Img1, Img2 }) => {
     useEffect(() => {
         Aos.init({ duration: 1000 });
     }, []);
+
     const [isSidebarVisible, setSidebarVisible] = useState(false);
 
     // Function to toggle sidebar visibility
@@ -18,14 +19,26 @@ const SideBar = ({ key, selectedState, selectedCapital, selectedBiome, selectedF
         setSidebarVisible(!isSidebarVisible);
     };
 
-    const [carouselImages, setCarouselImages] = useState([
-        Img1,
-        Img2
-    ]);
+    const [carouselImages, setCarouselImages] = useState([Img1, Img2]);
+
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1,
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+        },
+    };
+
     return (
         <div className={`container ${isSidebarVisible ? "visible" : ""}`}>
             <div className={`top ${isSidebarVisible ? "visible" : ""}`}>
-
                 <FontAwesomeIcon icon={faEarthAmericas} style={{ color: "#ffffff" }} size="2x" />
                 <h1>Mapa</h1>
                 <FontAwesomeIcon
@@ -39,41 +52,39 @@ const SideBar = ({ key, selectedState, selectedCapital, selectedBiome, selectedF
             <div className={`info ${isSidebarVisible ? "visible" : ""}`}>
                 <div className={`infochild ${isSidebarVisible ? "visible" : ""}`}>
                     <h5 className={`h5c ${isSidebarVisible ? "visible" : ""}`}>Estado:</h5>
-                    <p className={`pc ${isSidebarVisible ? "visible" : ""}`} data-aos="zoom-out">• {selectedState}</p>
+                    <p className={`pc ${isSidebarVisible ? "visible" : ""} green`} data-aos="zoom-out">• {selectedState}</p>
                 </div>
                 <div className={`infochild ${isSidebarVisible ? "visible" : ""}`}>
                     <h5 className={`h5c ${isSidebarVisible ? "visible" : ""}`}>Capital:</h5>
-                    <p className={`pc ${isSidebarVisible ? "visible" : ""}`} data-aos="zoom-out">• {selectedCapital}</p>
+                    <p className={`pc ${isSidebarVisible ? "visible" : ""} blue`} data-aos="zoom-out">• {selectedCapital}</p>
                 </div>
                 <div className={`infochild ${isSidebarVisible ? "visible" : ""}`}>
                     <h5 className={`h5c ${isSidebarVisible ? "visible" : ""}`}>Bioma:</h5>
-                    <p className={`pc ${isSidebarVisible ? "visible" : ""}`} data-aos="zoom-out">• {selectedBiome}</p>
+                    <p className={`pc ${isSidebarVisible ? "visible" : ""} yellow`} data-aos="zoom-out">• {selectedBiome}</p>
                 </div>
                 <div className={`infochild ${isSidebarVisible ? "visible" : ""}`}>
                     <h5 className={`h5c ${isSidebarVisible ? "visible" : ""}`}>Fuso-Horário:</h5>
-                    <p className={`pc ${isSidebarVisible ? "visible" : ""}`} data-aos="zoom-out">• {selectedFuso}</p>
+                    <p className={`pc ${isSidebarVisible ? "visible" : ""} red`} data-aos="zoom-out">• {selectedFuso}</p>
                 </div>
                 <div className={`infochild ${isSidebarVisible ? "visible" : ""}`}>
                     <h5 className={`h5c ${isSidebarVisible ? "visible" : ""}`}>Habitantes:</h5>
-                    <p className={`pc ${isSidebarVisible ? "visible" : ""}`} data-aos="zoom-out">• {selectedPopul}</p>
+                    <p className={`pc ${isSidebarVisible ? "visible" : ""} purple`} data-aos="zoom-out">• {selectedPopul}</p>
                 </div>
                 <div className={`pictures ${isSidebarVisible ? 'visible' : ''}`}>
                     <h2 className={`h2c ${isSidebarVisible ? 'visible' : ''}`}> Fotos </h2>
                     <div className={`picContainer ${isSidebarVisible ? 'visible' : ''}`}>
-                        <Carousel  className={`car ${isSidebarVisible ? 'visible' : ''}`} showThumbs={false}>
+                        <Carousel responsive={responsive} className={`carrouself ${isSidebarVisible ? 'visible' : ''}`}>
                             {carouselImages.map((imageUrl, index) => (
                                 <div key={index}>
-                                    <img src={imageUrl} alt={`Image ${index}`} className="imgcarousel"/>
+                                    <img src={imageUrl} alt={`Image ${index}`} className="imgcarousel" />
                                 </div>
                             ))}
                         </Carousel>
                     </div>
                 </div>
-
             </div>
         </div>
     );
 }
-
 
 export default SideBar;
